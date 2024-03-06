2f0ac92e6aeb9618571398be1db0e9c3f48a5f99
razahaider03 <54476563+razahaider03@users.noreply.github.com>
2024-03-06 04:31:32 +0000

    add Public API

--- Section-29-Using-Public-API/index.js
+++ Section-29-Using-Public-API/index.js
@@ -3,13 +3,18 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-
+const API_URL = "https://v2.jokeapi.dev/joke/Any?type=twopart"
 app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs", {port: port})
+app.get("/", async (req, res) => {
+    const result = await axios.get(API_URL)
+    console.log(result.data)
+    res.render("index.ejs", {
+        joke: result.data.setup,
+        delivery: result.data.delivery,
+    })
 })
 
-app.listen(port,() =>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
