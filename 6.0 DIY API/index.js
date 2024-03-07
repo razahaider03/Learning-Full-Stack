4099134b636160054283a6aded44f292831dcf76
razahaider03 <54476563+razahaider03@users.noreply.github.com>
2024-03-07 12:15:54 +0000

    Add GET Route

--- 6.0 DIY API/index.js
+++ 6.0 DIY API/index.js
@@ -8,6 +8,10 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+app.get("/random", (req,res)=>{
+  const randIndex = Math.floor(Math.random()*jokes.length);
+  res.json(jokes[randIndex]);
+})
 
 //2. GET a specific joke
 
